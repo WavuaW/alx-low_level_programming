32be504b3d5cec19773a2293cf1e87d269eb3b4f
Christine Wavua <wavuamuroka@gmail.com>
2022-06-30 07:26:08 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main - block main to be called
  * Return: 0 for set int
  */
- int main(void)
- {
-	 puts("\"Programming is like building a multilingual puzzle");
-	 return (0);	  
- }
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);	  
+}
