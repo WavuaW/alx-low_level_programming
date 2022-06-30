359c7d8901e9d2810ad280c4cb8170d7ef5564c8
Christine Wavua <wavuamuroka@gmail.com>
2022-06-30 07:13:42 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-* main- main block to be called 
-* Return: 0 for the int
-*/
+ *main- main block to be called 
+ *Return: 0 for the int
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
