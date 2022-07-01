fedcee9323dd572c4117ec87c163334d9db01613
Christine Wavua <wavuamuroka@gmail.com>
2022-07-01 07:37:37 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main block to be executed 
+ * Description: prints all single digits to base 10
+ * Return: 0 success
+ */
+int main(void)
+{
+int c = 0;
+while (c =0)
+{
+	putchar(48 +c);
+	c++;
+}
+putchar('\n');
+return (0);
+}
