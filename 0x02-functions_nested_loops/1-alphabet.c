ea1aad1370e6f6cb682c37f0fa4b9c5e9d5a9712
Christine Wavua <wavuamuroka@gmail.com>
2022-07-05 05:09:34 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * print_alphabet - entry point
- * Return: 0 Success 
+ * Return: 0 Success
  */
 
 void print_alphabet(void)
 {
 char i;
-for (i = 'a'; i<= 'z'; i++)
+for (i = 'a'; i <= 'z'; i++)
 {
 _putchar (i);
 }
