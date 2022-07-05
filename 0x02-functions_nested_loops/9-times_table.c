c14c1d9a4e51632e2e06fa39ba3485e93f6330fa
Christine Wavua <wavuamuroka@gmail.com>
2022-07-05 07:23:38 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ while (a <= 9)
 	b = 0;
 	while (b <= 9)
 	{
-		rep = a * b;
+		rep = a* b;
 		if (b == 0)
 		{
 			_putchar('0' + rep);
@@ -26,17 +26,17 @@ while (a <= 9)
 		else
 		{
 			_putchar('0' + rep / 10);
-			_putchar('0' + rep % 10;
+			_putchar('0' + rep % 10);
 		}
 		if (b < 9)
 		{
-		        _putchar(',')
-			_putchar(' ')
+			_putchar(',');
+			_putchar(' ');
 		}
 		b++;
 	}
 	_putchar('\n');
 	a++;
 }
-}
 
+}
