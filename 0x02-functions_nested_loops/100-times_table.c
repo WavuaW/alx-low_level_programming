16612e17d8331ecf8fef9c027bf7327f44fb520e
Christine Wavua <wavuamuroka@gmail.com>
2022-07-05 10:47:46 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,6 +10,7 @@ void print_times_table(int n)
 int a = 0, rep, b;
 if (n < 0 || n > 15)
 	return;
+
 while (a <= n)
 {
 	for (b = 0; b <= n; b++)
@@ -23,16 +24,16 @@ while (a <= n)
 			_putchar(' ');
 			_putchar('0' + rep);
 		}
-		else if (rep < 100)
+		else if ( rep < 100)
 		{
-			_putchar (' ');
+			_putchar(' ');
 			_putchar('0' + rep / 10);
-			_Putchar('0' + rep % 10);
+			_putcahr('0' + rep % 10);
 		}
-		else 
+		else
 		{
 			_putchar('0' + rep / 100);
-			_putchar('0' + (rep - 100) / 10);
+			_putchar('0' + (rep - 100) /10);
 			_putchar('0' + rep % 10);
 		}
 		if (b < n)
