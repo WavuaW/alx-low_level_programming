0ccb961c3c175ce7a7f6eb4c543b53e3afc2e1fa
Christine Wavua <wavuamuroka@gmail.com>
2022-07-11 10:51:01 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _puts - prints str followed by a new line
- * @str: pointer to the string to point 
- * Return: void 
+ * @str: pointer to the string to point
+ * Return: void
  */
 
 void _puts(char *str)
