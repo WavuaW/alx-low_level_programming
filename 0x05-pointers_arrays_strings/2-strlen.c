9ce4ad44e0a2313af98a1fcc7ecd5b5dd21401dc
Christine Wavua <wavuamuroka@gmail.com>
2022-07-11 10:41:35 -0700

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _strlem - finds the length of the string
- * @s: pointer for the string to be checked 
+ * _strlen - finds the length of the string
+ * @s: pointer for the string to be checked
  * Return: void
  */
 
