915e253765310c82a5b85437a039df2c89bca428
Christine Wavua <wavuamuroka@gmail.com>
2022-07-14 01:30:43 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncat: bytes of 2 strings
+ * @dest: ponter destination
+ * @src:P source
+ * @n: No. of bytes
+ * Return: void
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_leng, i;
+
+	for (dest_leng = 0; dest[dest_leng] != '\0'; dest_leng++)
+	;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_leng + i] = src[i];
+
+	dest[dest_leng + i] = '\0';
+
+	return (dest);
+}
