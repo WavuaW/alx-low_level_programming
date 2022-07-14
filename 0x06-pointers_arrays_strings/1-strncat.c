e3d2aad6347e40afb33b052655ff2ab206578dd4
Christine Wavua <wavuamuroka@gmail.com>
2022-07-14 01:42:59 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,17 +6,18 @@
  * @n: No. of bytes
  * Return: void
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_leng, i;
+	int dest_len, i;
 
-	for (dest_leng = 0; dest[dest_leng] != '\0'; dest_leng++)
+	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
 	;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[dest_leng + i] = src[i];
+		dest[dest_len + i] = src[i];
 
-	dest[dest_leng + i] = '\0';
+	dest[dest_len + i] = '\0';
 
 	return (dest);
 }
