8f2ea93f4455e7ebaca6999d5da6ef7a1581e27f
Christine Wavua <wavuamuroka@gmail.com>
2022-08-01 14:39:31 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+
+#define DOG_H
+
+/**
+ * struct dog - structure
+ * @name: string
+ * @age: float
+ * @owner: characters
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+#endif
