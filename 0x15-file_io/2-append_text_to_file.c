d280bb0475c47d65918dc2d48a8439d860651633
Christine Wavua <wavuamuroka@gmail.com>
2022-08-22 11:11:18 -0700

    Mandatory

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,31 +1,33 @@
-#include "main.h"
+#include "file.h"
 
 /**
- * append_text_to_file.c - function that creates a file
- * @filename: name of the file to append
- * @text_content: NULL terminated string to append to the file
- * Return: -1 if filename is NULL and 1 if successful
+ * append_text_to_file - append text to file
+ * @filename: -
+ * @text_content: -
+ * Return: Success ? 1 : -1
  */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-        int fungua, andika, urefu = 0;
-        if (filename == NULL)
-                return (-1);
+	int file_desc, _write;
 
-        if (text_content != NULL)
-        {
-                for (urefu = 0; text_content[urefu];)
-                        urefu++;
-        }
+	file_desc = open(filename, O_WRONLY | O_APPEND);
+	if (file_desc == -1)
+		return (-1);
 
-        fungua = open(filename, O_WRONLY | O_APPEND);
-        andika = write(fungua, text_content, urefu);
+	if (!text_content)
+	{
+		close(file_desc);
+		return (1);
+	}
 
-        if (fungua == -1 || andika == -1)
-                return (-1);
+	_write = write(file_desc, text_content, slen(text_content));
+	if (_write == -1 || _write != slen(text_content))
+	{
+		close(file_desc);
+		return (-1);
+	}
 
-        close(fungua);
-
-        return (1);
+	close(file_desc);
+	return (1);
 }
