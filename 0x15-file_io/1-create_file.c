9e55bc9a406da3b34cea94d543341f80e7d961dc
Christine Wavua <wavuamuroka@gmail.com>
2022-08-22 10:48:34 -0700

    Mandatiry

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,45 +1,31 @@
 #include "main.h"
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 
 /**
- * _strlen - finds the length of a string
- * @str: pointer to the string
- *
- * Return: length of the string
- */
-size_t _strlen(char *str)
-{
-	size_t i;
-
-	for (i = 0; str[i]; i++)
-		;
-	return (i);
-}
-
-/**
- * create_file - creates a file.
+ * create_file - function that creates a file
  * @filename: name of the file to create
  * @text_content: NULL terminated string to write to the file
- *
- * Return: 1 on success, -1 on failure
+ * Return: -1 if filename is NULL and 1 if successful
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t len = 0;
+	int fungua, andika, urefu = 0;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
-		return (-1);
+
 	if (text_content != NULL)
-		len = write(fd, text_content, _strlen(text_content));
-	close(fd);
-	if (len == -1)
+	{
+		for (urefu = 0; text_content[urefu];)
+			urefu++;
+	}
+
+	fungua = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	andika = write(fungua, text_content, urefu);
+
+	if (fungua == -1 || andika == -1)
 		return (-1);
+
+	close(fungua);
 	return (1);
 }
